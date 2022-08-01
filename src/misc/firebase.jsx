9876571519151c804ae105dcfbac16f8a430d535
rahul <rahulslg20@gmail.com>
2022-08-02 01:24:44 +0530

    joined firebase

---
+++ src/misc/firebase.jsx
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyCxIirDyaVscWDf51nHTt61yqu_c_z5yso',
+  authDomain: 'chat-web-app-3c4fa.firebaseapp.com',
+  projectId: 'chat-web-app-3c4fa',
+  storageBucket: 'chat-web-app-3c4fa.appspot.com',
+  messagingSenderId: '852745850370',
+  appId: '1:852745850370:web:df739322d15cc17a8f211b',
+};
+
+const app = firebase.initializeApp(config);
