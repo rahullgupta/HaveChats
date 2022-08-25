52453f4fd6327de93aeaa85b34edc02aa64298ec
rahul <rahulslg20@gmail.com>
2022-08-25 20:45:32 +0530

    Displayed user avatar

--- src/components/dashboard/AvatarUploadBtn.jsx
+++ src/components/dashboard/AvatarUploadBtn.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Modal, useToaster, Message, Button } from 'rsuite';
 import AvatarEditor from 'react-avatar-editor';
 import { useModalState } from '../../misc/custom-hooks';
+import { database, storage } from '../../misc/firebase';
+import { useProfile } from '../../context/profile.context';
+import ProfileAvatar from '../ProfileAvatar';
 
 const fileInputTypes = '.png, .jpeg, .jpg';
 
@@ -9,12 +12,27 @@ const acceptedFileTypes = ['image/png', 'image/jpeg', 'image/jpg'];
 
 const isValidFile = file => acceptedFileTypes.includes(file.type);
 
+const getBlob = canvas => {
+  return new Promise((resolve, reject) => {
+    canvas.toBlob(blob => {
+      if (blob) resolve(blob);
+      else reject(new Error('File process error'));
+    });
+  });
+};
+
 function AvatarUploadBtn() {
   const { isOpen, open, close } = useModalState();
+
+  const { profile } = useProfile();
 
   const toaster = useToaster();
 
   const [img, setImg] = useState(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const avatarEditorRef = useRef();
 
   const onFileInputChange = ev => {
     const currFiles = ev.target.files;
@@ -34,8 +52,43 @@ function AvatarUploadBtn() {
     }
   };
 
+  const onUploadClick = async () => {
+    const canvas = avatarEditorRef.current.getImageScaledToCanvas();
+    setIsLoading(true);
+    try {
+      const blob = await getBlob(canvas);
+      const avatarFileRef = storage
+        .ref(`/profiles/${profile.uid}`)
+        .child('avatar');
+      const uploadAvatarResult = await avatarFileRef.put(blob, {
+        cacheControl: `public, max-age=${3600 * 24 * 3}`,
+      });
+      const downloadUrl = await uploadAvatarResult.ref.getDownloadURL();
+      const userAvatarRef = database
+        .ref(`/profiles/${profile.uid}`)
+        .child('avatar');
+      userAvatarRef.set(downloadUrl);
+      setIsLoading(false);
+      const message = (
+        <Message showIcon type="info">
+          Avatar has been uploaded
+        </Message>
+      );
+      toaster.push(message);
+    } catch (err) {
+      setIsLoading(false);
+      const message = (
+        <Message showIcon type="info">
+          {err.message}
+        </Message>
+      );
+      toaster.push(message);
+    }
+  };
+
   return (
     <div className="mt-3 text-center">
+      <ProfileAvatar src={profile.avatar} name={profile.name} className="width-200 height-200 img-fullsize font-huge" />
       <div>
         <label
           htmlFor="avatar-upload"
@@ -56,6 +109,7 @@ function AvatarUploadBtn() {
             <div className="d-flex justify-content-center align-items-center h-100">
               {img && (
                 <AvatarEditor
+                  ref={avatarEditorRef}
                   image={img}
                   width={200}
                   height={200}
@@ -67,7 +121,12 @@ function AvatarUploadBtn() {
             </div>
           </Modal.Body>
           <Modal.Footer>
-            <Button block appearance="ghost">
+            <Button
+              block
+              appearance="ghost"
+              onClick={onUploadClick}
+              disabled={isLoading}
+            >
               Upload new Avatar
             </Button>
           </Modal.Footer>
