ba47c33619539c3b0114864016062a408b43e649
rahul <rahulslg20@gmail.com>
2022-08-29 00:10:04 +0530

    Created rooms context

--- src/misc/helpers.jsx
+++ src/misc/helpers.jsx
@@ -3,3 +3,11 @@ export function getNameInitials(name) {
   if (splitName.length > 1) return splitName[0][0] + splitName[1][0];
   return splitName[0][0];
 }
+
+export function transformToArrWithId(snapVal) {
+  return snapVal
+    ? Object.keys(snapVal).map(roomId => {
+        return { ...snapVal[roomId], id: roomId };
+      })
+    : {};
+}
